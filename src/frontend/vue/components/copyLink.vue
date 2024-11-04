5e049f349ae24c576a3faa7afeab7cb381abb352
gz <grg.zebrowski@gmail.com>
2024-11-04 16:53:53 +0100

    copy link component

--- src/frontend/vue/components/copyLink.vue
+++ src/frontend/vue/components/copyLink.vue
@@ -1,23 +1,36 @@
 <template>
-<div :class="cls || ''"><a v-acc-key @click="copyUrl()" class="pointer"><slot></slot></a><div
-  class="alert alert-outline-info border" v-if="showMessage">Link został skopiowany</div></div>
+<div class="copy-link-com">
+  <div class="p-relative" :class="cls || ''">
+    <input type="text" v-model="urlToCopy" readonly class="d-inline-block w-100 pr-4">
+    <span class="copy-msg" v-if="showMessage">Copied</span>
+    <a @click="copyUrl()" class="pointer p-absolute abs-right z9 d-inline-block icon-holder">
+      <svg viewBox="0 0 512 512">
+        <g data-name="1" id="_1">
+          <path d="M308.51,450H80.59a15,15,0,0,1-15-15V143.93a15,15,0,0,1,15-15H308.51a15,15,0,0,1,15,15V435A15,15,0,0,1,308.51,450ZM95.59,420H293.51V158.93H95.59Z"/><path d="M389.44,369.07H308.51a15,15,0,0,1,0-30h65.93V78H176.52v65.92a15,15,0,0,1-30,0V63a15,15,0,0,1,15-15H389.44a15,15,0,0,1,15,15V354.07A15,15,0,0,1,389.44,369.07Z"/>
+        </g>
+      </svg>
+    </a>
+  </div>
+</div>
 </template>
 <script>
 
-/*
-Properties: 'url', 'cls', 'alert'
-This just generates link that when clicked it copies given url to clipboard. If not provided, then current url
-is used. If alert is provided it will show app alert with the provided message. Else the default message will be shown.
-This directive uses the content as the label of the link.
-*/
-
 export default {
   name: 'copyLink',
-  props: ['url', 'cls', 'alert'],
+  props: ['url', 'cls'],
   data() {
     return {
       showMessage: false,
+      urlToCopy: '',
     };
+  },
+  created() {
+    this.urlToCopy = this.url;
+  },
+  watch: {
+    url() {
+      this.urlToCopy = this.url;
+    },
   },
   methods: {
     copyUrl() {
